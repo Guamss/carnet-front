312a4c32da26a5fbed03c62d2a4391ec398ff24d
lmerkel <lmerkel@unistra.fr>
2026-03-18 14:23:10 +0100

    add dialog

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,34 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+const dialogRef = ref<HTMLDialogElement | null>(null)
+
+const showDialog = () => {
+  dialogRef.value?.showModal()
+}
+const closeDialog = () => {
+  dialogRef.value?.close()
+}
+</script>
 
 <template>
   <h2>Liste des carnets :</h2>
+  <dialog ref="dialogRef">
+    <form>
+      <p>test</p>
+    </form>
+    <button @click="closeDialog">Fermer</button>
+  </dialog>
+  <div>
+    <button @click="showDialog">+ Crée un Carnet</button>
+  <form>
+    <label>Filtrer par nom :</label>
+    <label>Filtrer par tags :</label>
+    <input type="text" placeholder="abc.." required />
+  </form>
+  </div>
+  <section>
+  <!-- TODO Boite -->
+  </section>
 </template>
 
 <style scoped></style>
