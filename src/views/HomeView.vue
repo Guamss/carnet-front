b00fdd9e0c8201508d7a5c517deb8f30f01faf23
lmerkel <lmerkel@unistra.fr>
2026-03-18 16:28:51 +0100

    added ref in homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,32 +2,53 @@
 import { ref } from 'vue'
 const dialogRef = ref<HTMLDialogElement | null>(null)
 
+const newQuote = ref<string>('')
+const newAuthor = ref<string>('')
+const newLabels = ref<Array<string>>([''])
+
 const showDialog = () => {
   dialogRef.value?.showModal()
 }
 const closeDialog = () => {
   dialogRef.value?.close()
 }
+
+function createQuote() {
+  console.log(newQuote.value, newAuthor.value, newLabels.value)
+}
 </script>
 
 <template>
   <h2>Liste des carnets :</h2>
   <dialog ref="dialogRef">
-    <form>
-      <p>test</p>
+    <form @submit.prevent="createQuote">
+      <p>Nouveau carnet</p>
+      <label>
+        Carnet :
+        <input v-model="newQuote" type="text" id="create-quote" required />
+      </label>
+      <label>
+        Auteur :
+        <input v-model="newAuthor" type="text" id="create-author" required />
+      </label>
+      <label>
+        Associer un label :
+        <input v-model="newLabels" type="text" id="create-label" required />
+      </label>
+      <button type="submit" @submit="createQuote">Ajouter</button>
+      <button @click="closeDialog">Fermer</button>
     </form>
-    <button @click="closeDialog">Fermer</button>
   </dialog>
   <div>
     <button @click="showDialog">+ Crée un Carnet</button>
     <div>
       <label
         >Filtrer par nom :
-        <input type="text" id="quote" placeholder="abc.." />
+        <input type="text" id="filter-quote" placeholder="abc.." />
       </label>
       <label
         >Filtrer par tags :
-        <input type="text" id="labels" />
+        <input type="text" id="filter-labels" />
       </label>
     </div>
   </div>
@@ -37,14 +58,13 @@ const closeDialog = () => {
 </template>
 <style scoped>
 main {
-
   > div {
     display: flex;
     flex-direction: column;
 
     > div {
       display: flex;
-      gap:1.5rem;
+      gap: 1.5rem;
     }
   }
 }
